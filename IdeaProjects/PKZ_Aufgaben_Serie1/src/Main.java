0a83914e7b68add5e326fb0a23525744b624b14c
Joachimstaller Maximilian <1810653613@stud.fh-kufstein.ac.at>
2018-11-08 10:41:10 +0100

    Aufgabe 2

--- IdeaProjects/PKZ_Aufgaben_Serie1/src/Main.java
+++ IdeaProjects/PKZ_Aufgaben_Serie1/src/Main.java
@@ -1,11 +1,11 @@
-public class Main
+public class Main  //Das Programm ruft die Main Methode auf
 {
-    public static void main(String[] args)
+    public static void main(String[] args) //Einstiegspunkt in das Programm
 
     {
-        System.out.println("Hello World!");
-        System.out.println("Max Joachimstaller");
-        System.out.println("hi");
+        System.out.println("Hello World!"); //Über den Befehl "System.out.Println("") werden die in Anführungszeichen eingegebenen Werte in die Konsole geschrieben.
+        System.out.println("Max Joachimstaller"); //Über den Befehl "System.out.Println("") werden die in Anführungszeichen eingegebenen Werte in die Konsole geschrieben.
+        System.out.println("hi");  //Über den Befehl "System.out.Println("") werden die in Anführungszeichen eingegebenen Werte in die Konsole geschrieben.
 
     }
 }
--- IdeaProjects/PKZ_Aufgaben_Serie1/src/package2/Main2.java
+++ IdeaProjects/PKZ_Aufgaben_Serie1/src/package2/Main2.java
@@ -1,11 +1,11 @@
-package package2;
+package package2; //Package 2 wird geladen
 
-public class Main2
+public class Main2 // Die Main Methode wird aufgerufen
 {
-    public static void main(String[] args)
+    public static void main(String[] args) //Einstiegspunkt in das Programm
 
     {
-        System.out.println("FH Kufstein Tirol");
+        System.out.println("FH Kufstein Tirol"); //Über den Befehl "System.out.Println("") werden die in Anführungszeichen eingegebenen Werte in die Konsole geschrieben.
 
     }
 
