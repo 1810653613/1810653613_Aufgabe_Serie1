0dad61045727b2a309db1eee50de63b05e748b04
Joachimstaller Maximilian <1810653613@stud.fh-kufstein.ac.at>
2018-11-08 11:18:37 +0100

    Aufgabe 5

--- IdeaProjects/PKZ_Aufgaben_Serie1/src/package3/Main3.java
+++ IdeaProjects/PKZ_Aufgaben_Serie1/src/package3/Main3.java
@@ -1,17 +1,17 @@
 package package3;
 
-public class Main3
-{
+public class Main3 {
     public static void main(String[] args)
 
     {
-                String name = "Max Joachimstaller" ;
-                final String FH_KUFSTEIN_TIROL = "Fachhochschule Kufstein Tirol";  //Variablennamen sinnvoll auf fhKufsteinTirol verkürzt
-                //Variable durch Schlüsselwort final in eine Konstante verwandelt und Namen auf FH_KUFSTEIN_TIROL umbennant
-                int alter = 21 ;
-                System.out.println(name);
-                System.out.println(alter);
-
+        String name = "Max Joachimstaller";
+        final String adresse = "";
+        final String FH_KUFSTEIN_TIROL = "Fachhochschule Kufstein Tirol";  //Variablennamen sinnvoll auf fhKufsteinTirol verkürzt
+        //Variable durch Schlüsselwort final in eine Konstante verwandelt und Namen auf FH_KUFSTEIN_TIROL umbennant
+        int alter = 21;
+        System.out.println(name);
+        System.out.println(alter);
+         //strg+alt+L formatiert alle zeilen in der main methode
     }
 
 }
