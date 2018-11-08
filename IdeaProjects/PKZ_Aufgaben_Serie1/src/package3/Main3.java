d45518756559a7b2715ff11187e549867ececac7
Joachimstaller Maximilian <1810653613@stud.fh-kufstein.ac.at>
2018-11-08 11:05:36 +0100

    Aufgabe 3

---
+++ IdeaProjects/PKZ_Aufgaben_Serie1/src/package3/Main3.java
@@ -0,0 +1,17 @@
+package package3;
+
+public class Main3
+{
+    public static void main(String[] args)
+
+    {
+                String name = "Max Joachimstaller" ;
+                int alter = 21 ;
+                System.out.println(name);
+                System.out.println(alter);
+
+    }
+
+}
+
+
