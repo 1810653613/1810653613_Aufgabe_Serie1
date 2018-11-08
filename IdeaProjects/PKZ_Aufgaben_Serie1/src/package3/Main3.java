edbfebac95749f7c27609d277e07de43cd5f5ef6
Joachimstaller Maximilian <1810653613@stud.fh-kufstein.ac.at>
2018-11-08 11:42:27 +0100

    Aufgabe 6

--- IdeaProjects/PKZ_Aufgaben_Serie1/src/package3/Main3.java
+++ IdeaProjects/PKZ_Aufgaben_Serie1/src/package3/Main3.java
@@ -12,6 +12,12 @@ public class Main3 {
         System.out.println(name);
         System.out.println(alter);
          //strg+alt+L formatiert alle zeilen in der main methode
+        System.out.println("Java verwendet für Strings \"doppelte Anführungszeichen\" und einzelne Anführungszeichen wie \'c\' um einzelne Zeichen zu definieren.");
+        System.out.println("Durch \\ kann man auch den Backslash selbst beliebig oft ausgeben: \\\\\\\\ ");
+        System.out.println("Ich verwende den\b Backspace, die Newline\n und den \tTabulator");
+        System.out.println("Durch den Wagenrücklauf wird die Zeile zurückgesetzt und der Teil nach dem \r überschreibt die bestehende Zeile. \fist in der Konsole nicht bemerkbar, da es einen Seitenumbruch erzwingt");
+
+
     }
 
 }
